78cc60b0b632c3a772d9c305fc36f8f783df7969
mattj122 <mattj122@users.noreply.github.com>
2018-03-22 16:46:05 -0500

    Properties Files

--- Java/Project Files/src/prop_file/DriverTester.java
+++ Java/Project Files/src/prop_file/DriverTester.java
@@ -5,12 +5,24 @@ import java.util.Properties;
 
 public class DriverTester {
 	static Properties prop = new Properties();
-	static File fileTest = new File("C:\\Users\\vv3383my\\Documents\\GitHub\\Resources\\Java\\Project Files\\bin\\prop_file\\test.properties");
+	static File fileTest = new File("C:\\Users\\vv3383my\\Documents\\GitHub\\Resources\\Java\\Project Files\\src\\prop_file\\prop_test.ini");
 	public static void main(String[] args) throws IOException {
+		
+		
 		fileLoadProperties(prop, fileTest);
-		System.out.println("Property = " + prop.getProperty("key2"));
+		
+		String propName = "version";
+		System.out.println(propName + "=" + prop.getProperty(propName, "unknown version"));
+		propName = "appName";
+		System.out.println(propName + "=" + prop.getProperty(propName, "unknown name"));
+		propName = "datePublished";
+		System.out.println(propName + "=" + prop.getProperty(propName, "unknown date"));
+		
+		prop.list(System.out);
+		
+		//fileSaveProperties(prop, fileTest);
 	}
-	public void fileSaveProperties(Properties p, File f) throws IOException{
+	public static void fileSaveProperties(Properties p, File f) throws IOException{
 		try {
 			FileOutputStream out = new FileOutputStream(f);
 			p.store(out, "--No Comment--");
